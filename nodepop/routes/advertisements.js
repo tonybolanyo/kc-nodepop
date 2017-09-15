8586b7906afacea68c4a687b27fd107e93d233b4
Antonio G. Bolaño <angonzalez@odec.es>
2017-09-15 15:03:02 +0200

    Testing filters syntax

--- nodepop/routes/advertisements.js
+++ nodepop/routes/advertisements.js
@@ -1,8 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const logger = require('morgan');
 
 router.get('/', (req, res) => {
-    res.json({ todo: 'List of advertisements '});
+    const tag = req.query.tag;
+    const isSale = req.query.sale;
+    let filter = {}
+    if (tag) {
+        filter.tag = tag;
+    }
+    if (isSale) {
+        filter.sale = isSale;
+    }
+    console.log(req.query);
+    res.json({
+        todo: 'List of advertisements ',
+        filter: filter
+    });
 });
 
 router.post('/', (req, res) => {
