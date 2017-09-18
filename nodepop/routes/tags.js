54cb928d5132e468e8cdfe8bc7a4efee9ad5c235
Antonio G. Bolaño <angonzalez@odec.es>
2017-09-18 12:00:51 +0200

    Basic tags listing

--- nodepop/routes/tags.js
+++ nodepop/routes/tags.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const Advertisement = mongoose.model('Advertisement');
 
 router.get('/', (req, res, next) => {
-    res.json({ todo: 'List of tags' });
+    Advertisement.distinct('tags', (err, data) => {
+        if (err) {
+            console.error('Error listing tags', err);
+            res.status(500).json({
+                error: "Error listing tags",
+                details: err
+            });
+        }
+        console.log(data);
+        res.json({
+            tags: data
+        });
+    });
 });
 
 module.exports = router;
