9460d028b5e1da7485af5871b518b7a07f82a66f
Antonio G. Bolaño <angonzalez@odec.es>
2017-09-19 11:46:37 +0200

    Using promises to init advertisement collection

--- init_data.js
+++ init_data.js
@@ -13,21 +13,39 @@ const advCollection = 'advertisements';
 
 const connectionString = `mongodb://${mongoServer.url}:${mongoServer.port}/${mongoServer.database}`;
 
+let database;
+
 fs.readFile(dataFilePath, 'utf8', (err, data) => {
     if (err) {
         console.error("Error reading sample data file:", err);
         process.exit(1);
     }
     const sampleAdvertisements = JSON.parse(data);
-    MongoClient.connect(connectionString, (err, db) => {
-        const advertisements = db.collection(advCollection);
-        advertisements.insertMany(sampleAdvertisements, (err, result) => {
-            if (err) {
-                console.error("Error:", err);
-                process.exit(2);
-            }
-            console.log("Docs inserted count:", result.insertedCount);
-            db.close();
-        });
-    });    
+
+    MongoClient.connect(connectionString)
+        .then((db) => {
+            console.log("  Connected ...");
+            database = db;
+        })
+        .then(() => {
+            const advertisements = database.collection(advCollection);
+            console.log("  removing previous advertisements ...");
+            return advertisements.remove();
+        })
+        .then(() => {
+            console.log("  inserting sample data ...");
+            const advertisements = database.collection(advCollection);
+            return advertisements.insertMany(sampleAdvertisements);
+        })
+        .then(() => {
+            console.log("  closing database connection ...");
+            return database.close();
+        })
+        .then(() => {
+            console.log("  advertisements collection initialized.");
+        })
+        .catch(err => {
+            console.error("[ERROR] Can't setup initial data in database:", err);
+            process.exit(2);
+        })
 });
