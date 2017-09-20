fb2216ecb3299e63f9b4cca98cddcc0959330eda
Antonio G. Bolaño <angonzalez@odec.es>
2017-09-20 15:07:26 +0200

    Linting init database script

--- init_data.js
+++ init_data.js
@@ -7,7 +7,7 @@ const mongoServer = {
     url: '127.0.0.1',
     port: '27017',
     database: 'nodepop'
-}
+};
 // End of configuration data
 
 const advCollection = 'advertisements';
@@ -18,35 +18,35 @@ let database;
 
 fs.readFile(dataFilePath, 'utf8', (err, data) => {
     if (err) {
-        console.error("Error reading sample data file:", err);
+        console.error('Error reading sample data file:', err);
         process.exit(1);
     }
     const sampleAdvertisements = JSON.parse(data);
 
     MongoClient.connect(connectionString)
         .then((db) => {
-            console.log("  Connected ...");
+            console.log('  Connected ...');
             database = db;
         })
         .then(() => {
             const advertisements = database.collection(advCollection);
-            console.log("  removing previous advertisements ...");
+            console.log('  removing previous advertisements ...');
             return advertisements.remove();
         })
         .then(() => {
-            console.log("  inserting sample data ...");
+            console.log('  inserting sample data ...');
             const advertisements = database.collection(advCollection);
             return advertisements.insertMany(sampleAdvertisements);
         })
         .then(() => {
-            console.log("  closing database connection ...");
+            console.log('  closing database connection ...');
             return database.close();
         })
         .then(() => {
-            console.log("  advertisements collection initialized.");
+            console.log('  advertisements collection initialized.');
         })
         .catch(err => {
-            console.error("[ERROR] Can't setup initial data in database:", err);
+            console.error('[ERROR] Can\'t setup initial data in database:', err);
             process.exit(2);
-        })
+        });
 });
