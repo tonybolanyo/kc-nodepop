2fa98b51c6e43d89b4fc107a2b6186f6def9e913
Tony G. Bolaño <tonybolanyo@gmail.com>
2017-11-04 08:56:03 +0100

    Add user/password to MongoDB connection string and create simple test connection script

--- config/database.js
+++ config/database.js
@@ -5,7 +5,9 @@ require('dotenv').config();
 module.exports = {
     mongodb: {
         host: process.env.NODEPOP_DBHOST || '127.0.0.1',
-        port: process.env.NODEPOP_PORT || '27017',
+        port: process.env.NODEPOP_DBPORT || '27017',
+        user: process.env.NODEPOP_DBUSER || '',
+        password: process.env.NODEPOP_DBPASSWORD || '',
         database: 'nodepop'
     }
 };
