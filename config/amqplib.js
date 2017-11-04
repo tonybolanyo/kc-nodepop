c10d35d5cdec3407c6b2fc2d0d1a844790e063b3
Tony G. Bolaño <tonybolanyo@gmail.com>
2017-11-05 00:58:12 +0100

    Fix #5 - #9 - #12

--- config/amqplib.js
+++ config/amqplib.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const amqplib = require('amqplib'); 
+require('dotenv').config();
+
+const amqplib = require('amqplib');
 
 const url = process.env.AMQP_URL;
-
 const connectionPromise = amqplib.connect(url)
     .catch(err => {
         console.error('[AMQP]', err);
--- config/multer.js
+++ config/multer.js
@@ -1,10 +1,21 @@
 'use strict';
 
+require('dotenv').config();
+
+const fs = require('fs');
+const path = require('path');
+
 const multer = require('multer');
+
+const uploadPath = path.join(__dirname, '..', process.env.NODEPOP_UPLOAD_FOLDER);
+
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath);
+}
 
 const storage = multer.diskStorage({
     destination: function(req, file, callback) {
-        callback(null, './uploads/');
+        callback(null, uploadPath);
     },
     filename: function(req, file, callback) {
         callback(null, Date.now() + '-' + file.originalname);
